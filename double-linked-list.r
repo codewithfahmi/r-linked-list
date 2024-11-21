cee790d2ff148de3cc8239c87bbfc98328054fce
codewithfahmi <fahmiyahya0@gmail.com>
2024-11-21 13:03:36 +0700

    First Commit

---
+++ double-linked-list.r
@@ -0,0 +1,219 @@
+mahasiswa <- function(
+    name,
+    address,
+    gender,
+    age,
+    hobbies,
+    gpa) {
+  env <- new.env()
+  env$name <- name
+  env$address <- address
+  env$gender <- gender
+  env$age <- age
+  env$hobbies <- hobbies
+  env$gpa <- gpa
+  env$to_string <- function() {
+    paste(
+      "name:", env$name,
+      "age:", env$age,
+      "gender:", env$gender,
+      "address:", env$address,
+      "hobbies:", toString(env$hobbies),
+      "gpa:", env$gpa
+    )
+  }
+  env
+}
+
+node <- function(item) {
+  env <- new.env()
+  env$item <- item
+  env$next_node <- NULL
+  env$previous_node <- NULL
+  env
+}
+
+linked_list <- function() { # nolint: cyclocomp_linter.
+  env <- new.env()
+
+  env$head <- NULL
+  env$tail <- NULL
+
+  env$append <- function(item) {
+    new_node <- node(item)
+    if (is.null(env$head)) {
+      env$tail <- new_node
+      env$head <- new_node
+    } else {
+      env$tail$next_node <- new_node
+      new_node$previous_node <- env$tail
+      env$tail <- new_node
+    }
+  }
+
+  env$prepend <- function(item) {
+    new_node <- node(item)
+    if (is.null(env$head)) {
+      env$head <- new_node
+      env$tail <- new_node
+    } else {
+      new_node$next_node <- env$head
+      env$head$previous_node <- new_node
+      env$head <- new_node
+    }
+  }
+
+  env$insert <- function(item, index) {
+    if (index < 0 || index > env$size()) {
+      return(cat("[WARN] index must be between 0 -", env$size(), "\n"))
+    }
+
+    if (index == 0) {
+      return(env$prepend(item))
+    }
+
+    if (index == env$size()) {
+      return(env$append(item))
+    }
+
+    new_node <- node(item)
+    current_node <- env$head
+    counter <- 0
+
+    while (!is.null(current_node) && counter < index - 1) {
+      current_node <- current_node$next_node
+      counter <- counter + 1
+    }
+
+    new_node$next_node <- current_node$next_node
+    current_node$next_node$previous_node <- new_node
+    current_node$next_node <- new_node
+    new_node$previous_node <- current_node
+  }
+
+  env$pop <- function() {
+    removed_item <- NULL
+
+    if (!is.null(env$tail)) {
+      removed_item <- env$tail$item
+      env$tail <- env$tail$previous_node
+    }
+
+    if (is.null(env$tail)) {
+      env$head <- NULL
+    } else {
+      env$tail$next_node <- NULL
+    }
+
+    return(removed_item)
+  }
+
+  env$unshift <- function() {
+    removed_item <- NULL
+
+    if (!is.null(env$head)) {
+      removed_item <- env$head$item
+      env$head <- env$head$next_node
+    }
+
+    if (is.null(env$head)) {
+      env$tail <- NULL
+    } else {
+      env$head$previous_node <- NULL
+    }
+
+    return(removed_item)
+  }
+
+  env$remove <- function(index) {
+    if (is.null(env$head)) {
+      return(NULL)
+    }
+
+    if (index < 0 || index > env$size() - 1) {
+      return(cat("[WARN] index must be between 0 -", env$size(), "\n"))
+    }
+
+    if (index == 0) {
+      return(env$unshift())
+    }
+
+    if (index == env$size() - 1) {
+      return(env$pop())
+    }
+
+    removed_item <- NULL
+
+    current_node <- env$head
+    counter <- 0
+    while (!is.null(current_node) && counter < index) {
+      current_node <- current_node$next_node
+      counter <- counter + 1
+    }
+
+    removed_item <- current_node$item
+    current_node$previous_node$next_node <- current_node$next_node
+    current_node$next_node$previous_node <- current_node$previous_node
+    current_node <- NULL
+    return(removed_item)
+  }
+
+  env$size <- function() {
+    current_node <- env$head
+    counter <- 0
+    while (!is.null(current_node)) {
+      counter <- counter + 1
+      current_node <- current_node$next_node
+    }
+    counter
+  }
+
+  env$all <- function() {
+    items <- list()
+    current_node <- env$head
+    while (!is.null(current_node)) {
+      append(items, current_node$item)
+      current_node <- current_node$next_node
+    }
+    return(items)
+  }
+
+  env$display <- function() {
+    current_node <- env$head
+    counter <- 0
+    while (!is.null(current_node)) {
+      cat(counter, "|", current_node$item$to_string(), "\n")
+      current_node <- current_node$next_node
+      counter <- counter + 1
+    }
+  }
+
+  env$display_backward <- function() {
+    current_node <- env$tail
+    counter <- 0
+    while (!is.null(current_node)) {
+      cat(counter, "|", current_node$item$to_string(), "\n")
+      current_node <- current_node$previous_node
+      counter <- counter + 1
+    }
+  }
+
+  env
+}
+
+ll <- linked_list()
+ll$prepend(
+  mahasiswa("fahmi", "jepara", "l", 24, list("makan", "tidur", "ngoding"), 2.4)
+)
+ll$prepend(
+  mahasiswa("siska", "jakarta", "p", 25, list("makan", "tidur", "ngoding"), 2.4)
+)
+ll$append(
+  mahasiswa("rudi", "bogor", "l", 19, list("makan", "tidur", "ngoding"), 2.4)
+)
+ll$insert(
+  mahasiswa("mail", "depok", "l", 30, list("makan", "tidur", "ngoding"), 2.4),
+  3
+)
+
+ll$display()
---
+++ linked-list.r
@@ -0,0 +1,195 @@
+mahasiswa <- function(
+    name,
+    address,
+    gender,
+    age,
+    hobbies,
+    gpa) {
+  env <- new.env()
+  env$name <- name
+  env$address <- address
+  env$gender <- gender
+  env$age <- age
+  env$hobbies <- hobbies
+  env$gpa <- gpa
+  env$to_string <- function() {
+    paste(
+      "name:", env$name,
+      "age:", env$age,
+      "gender:", env$gender,
+      "address:", env$address,
+      "hobbies:", toString(env$hobbies),
+      "gpa:", env$gpa
+    )
+  }
+  env
+}
+
+node <- function(item) {
+  env <- new.env()
+  env$item <- item
+  env$next_node <- NULL
+  env
+}
+
+linked_list <- function() { # nolint: cyclocomp_linter.
+  env <- new.env()
+
+  env$head <- NULL
+
+  env$append <- function(item) {
+    if (is.null(env$head)) {
+      return(env$prepend(item))
+    }
+
+    current_node <- env$head
+    while (!is.null(current_node$next_node)) {
+      current_node <- current_node$next_node
+    }
+    current_node$next_node <- node(item)
+    env$head
+  }
+
+  env$prepend <- function(item) {
+    new_node <- node(item)
+    new_node$next_node <- env$head
+    env$head <- new_node
+  }
+
+  env$insert <- function(item, index) {
+    if (index < 0 || index > env$size()) {
+      return(cat("[WARN] index must be between 0 -", env$size(), "\n"))
+    }
+
+    if (index == 0) {
+      return(env$prepend(item))
+    }
+
+    if (index == env$size()) {
+      return(env$append(item))
+    }
+
+    new_node <- node(item)
+    current_node <- env$head
+    counter <- 0
+
+    while (!is.null(current_node) && counter < index - 1) {
+      current_node <- current_node$next_node
+      counter <- counter + 1
+    }
+
+    new_node$next_node <- current_node$next_node
+    current_node$next_node <- new_node
+  }
+
+  env$pop <- function() {
+    if (is.null(env$head)) {
+      return(NULL)
+    }
+
+    if (is.null(env$head$next_node)) {
+      removed_item <- env$head$item
+      env$head <- NULL
+      return(removed_item)
+    }
+
+    current_node <- env$head
+    while (!is.null(current_node$next_node$next_node)) {
+      current_node <- current_node$next_node
+    }
+    removed_item <- current_node$next_node$item
+    current_node$next_node <- NULL
+    return(removed_item)
+  }
+
+  env$unshift <- function() {
+    if (is.null(env$head)) {
+      return(NULL)
+    }
+
+    removed_item <- env$head$item
+    env$head <- env$head$next_node
+    return(removed_item)
+  }
+
+  env$remove <- function(index) {
+    if (is.null(env$head)) {
+      return(NULL)
+    }
+
+    if (index < 0 || index > env$size() - 1) {
+      return(cat("[WARN] index must be between 0 -", env$size(), "\n"))
+    }
+
+    if (index == 0) {
+      return(env$unshift())
+    }
+
+    if (index == env$size() - 1) {
+      return(env$pop())
+    }
+
+    current_node <- env$head
+    counter <- 0
+    while (!is.null(current_node) && counter < index - 1) {
+      current_node <- current_node$next_node
+      counter <- counter + 1
+    }
+
+    removed_item <- current_node$next_node$item
+    current_node <- current_node$next_node$next_node
+    return(removed_item)
+  }
+
+  env$size <- function() {
+    current_node <- env$head
+    counter <- 0
+    while (!is.null(current_node)) {
+      counter <- counter + 1
+      current_node <- current_node$next_node
+    }
+    counter
+  }
+
+  env$all <- function() {
+    items <- list()
+    current_node <- env$head
+    while (!is.null(current_node)) {
+      append(items, current_node$item)
+      current_node <- current_node$next_node
+    }
+    return(items)
+  }
+
+  env$display <- function() {
+    current_node <- env$head
+    counter <- 0
+    while (!is.null(current_node)) {
+      cat(counter, "|", current_node$item$to_string(), "\n")
+      current_node <- current_node$next_node
+      counter <- counter + 1
+    }
+  }
+
+  env
+}
+
+ll <- linked_list()
+ll$prepend(
+  mahasiswa("fahmi", "jepara", "l", 24, list("makan", "tidur", "ngoding"), 2.4)
+)
+ll$prepend(
+  mahasiswa("siska", "jakarta", "p", 25, list("makan", "tidur", "ngoding"), 2.4)
+)
+ll$append(
+  mahasiswa("rudi", "bogor", "l", 19, list("makan", "tidur", "ngoding"), 2.4)
+)
+ll$insert(
+  mahasiswa("mail", "depok", "l", 30, list("makan", "tidur", "ngoding"), 2.4),
+  2
+)
+cat(ll$unshift()$to_string(), "\n")
+cat(ll$pop()$to_string(), "\n")
+cat(ll$unshift()$to_string(), "\n")
+cat(ll$pop()$to_string(), "\n")
+ll$display()
